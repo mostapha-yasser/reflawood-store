fdcd31e8415650b2f1cb5514778e4c01b32b6182
mostapha yasser <mostaphayasser18@gmail.com>
2025-09-01 22:11:07 +0200

    add : new section show top products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import { Suspense } from "react";
 import ProductCategories from "../components/HomeComponents/ ProductCategories";
 import HeroSection from "../components/HomeComponents/HeroSection";
 import TopProducts from "../components/HomeComponents/TopProducts";
 import WhyChooseUs from "../components/HomeComponents/WhyChooseUs";
+import Loading from "../components/ui/Loading";
 
 
 export default function Home() {
   return (
   <div className="w-4/5 mx-auto space-y-20 py-8 text-Text">
       <HeroSection />
+      <Suspense fallback={<Loading/>}>
+
       <TopProducts/>
+      </Suspense>
       <ProductCategories />
       <WhyChooseUs />
     </div>
--- src/components/HomeComponents/TopProducts.tsx
+++ src/components/HomeComponents/TopProducts.tsx
@@ -33,6 +33,7 @@ function TopProducts() {
   };
 
   return (
+    
     <section className="py-12">
       <h2 className="text-3xl font-bold text-center mb-8 text-main">
         Top Products

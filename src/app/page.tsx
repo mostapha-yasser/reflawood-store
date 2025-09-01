8f0c3dcfaf09de3aee69e74edc7716908a71b3ff
mostapha yasser <mostaphayasser18@gmail.com>
2025-09-01 22:02:15 +0200

    add : new section show top products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProductCategories from "../components/HomeComponents/ ProductCategories";
 import HeroSection from "../components/HomeComponents/HeroSection";
+import TopProducts from "../components/HomeComponents/TopProducts";
 import WhyChooseUs from "../components/HomeComponents/WhyChooseUs";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
   <div className="w-4/5 mx-auto space-y-20 py-8 text-Text">
       <HeroSection />
+      <TopProducts/>
       <ProductCategories />
       <WhyChooseUs />
     </div>
---
+++ src/components/HomeComponents/TopProducts.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Slider from "react-slick";
+import useProduct from "@/src/Hooks/useProduct";
+import Loading from "../ui/Loading";
+import ProductCard from "../productComponent/ProductCard";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function TopProducts() {
+  const { data, isLoading, error } = useProduct();
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading top products: {error.message}</div>;
+  if (!data || !Array.isArray(data)) return null;
+
+  const topProducts = data.filter((product) => product.isTopProduct);
+  if (topProducts.length === 0) return null;
+
+  const settings = {
+    dots: true,
+     arrows: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 640, settings: { slidesToShow: 1 } },
+    ],
+  };
+
+  return (
+    <section className="py-12">
+      <h2 className="text-3xl font-bold text-center mb-8 text-main">
+        Top Products
+      </h2>
+
+      <Slider {...settings}>
+        {topProducts.map((product) => (
+          <div key={product._id} className="px-2">
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </Slider>
+    </section>
+  );
+}
+
+export default TopProducts;
